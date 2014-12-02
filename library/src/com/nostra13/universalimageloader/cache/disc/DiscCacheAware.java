e7223a1394a25ac471038684936d4739a82e2254
iluxa-com <iluxa_com@mail.ru>
2014-12-02 17:14:36 +0300

    Update DiscCacheAware.java

--- library/src/com/nostra13/universalimageloader/cache/disc/DiscCacheAware.java
+++ library/src/com/nostra13/universalimageloader/cache/disc/DiscCacheAware.java
@@ -1,5 +1,8 @@
 /*******************************************************************************
  * Copyright 2011-2013 Sergey Tarasevich
+ * 
+ * 
+ * !!!!!!!!!!!
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
